822d8dc51db2db1d6f0660792a513aa7394a9cc5
aridguy <rotimiariyo@gmail.com>
2022-05-03 04:09:06 +0100

    not done with the page still modifying

--- travelagency/src/App.js
+++ travelagency/src/App.js
@@ -13,103 +13,146 @@ import Gpay from './assets/icons/apple-pay.png';
 import Start from './assets/icons/silverstar.png';
 import Intrax from './assets/icons/Intrax.png';
 import Master from './assets/icons/mastercard.png';
+
+import Arrow from './assets/icons/Arrowdown.svg';
+
+// import Bkga from './assets/Section1/bg-a.png';
+
 function App() {
   return (
     <>
 
-<nav className="navbar navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light" id="ftco-navbar">
-    <div className="container">
-      <a className="navbar-brand" href="index.html"><img src={Logo} /> <i>&nbsp; </i> Arctic Travels</a>
-        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav" aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
-        <span className="oi oi-menu"></span> Menu
-      </button>
+      <nav className="navbar navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light" id="ftco-navbar">
+        <div className="container">
+          <a className="navbar-brand" href="index.html"><img src={Logo} /> <i>&nbsp; </i> Arctic Travels</a>
+          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav" aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="oi oi-menu"></span> Menu
+          </button>
 
-      <div className="collapse navbar-collapse" id="ftco-nav">
-       <ul className="navbar-nav ml-auto nav-linkss">
-          <li className="nav-item"><a href="index.html" className="nav-link">About</a></li>
-          <li className="nav-item"><a href="about.html" className="nav-link">Support</a></li>
-          <li className="nav-item"><a href="contact.html" className="nav-link">Language</a></li>
-        </ul>
-        <button className='btn btn-primary signin-btn'>Sign in</button>
-      </div>
-    </div>
-  </nav>
-
-  <div className="hero-wrap heroimg"  data-stellar-background-ratio="0.5">
-      <div className="overlay"></div>
-      <div className="container">
-        <div className="row no-gutters slider-text align-items-center justify-content-center" data-scrollax-parent="true">
-          <div className='col-md-8'>
-            <h2 className='head-texts'>Plan The Perfect Winter Trip</h2>
-            <p className='sub-txt text-white'>Easily plan your ideal ski trip from home with the <br /> help of professionals</p>
+          <div className="collapse navbar-collapse" id="ftco-nav">
+            <ul className="navbar-nav ml-auto nav-linkss">
+              <li className="nav-item"><a href="index.html" className="nav-link">About</a></li>
+              <li className="nav-item"><a href="about.html" className="nav-link">Support</a></li>
+              <li className="nav-item"><a href="contact.html" className="nav-link">Language</a></li>
+            </ul>
+            <button className='btn btn-primary signin-btn'>Sign in</button>
           </div>
-          <div className='col-md-4'>
-        
+        </div>
+      </nav>
+
+      <div className="hero-wrap heroimg" data-stellar-background-ratio="0.5">
+        <div className="overlay"></div>
+        <div className="container">
+          <div className="row no-gutters slider-text align-items-center justify-content-center" data-scrollax-parent="true">
+            <div className='col-md-8'>
+              <h2 className='head-texts'>Plan The Perfect Winter Trip</h2>
+              <p className='sub-txt text-white'>Easily plan your ideal ski trip from home with the <br /> help of professionals</p>
+            </div>
+            <div className='col-md-4'>
+
+            </div>
+            <button className='book-btn'>Book Here</button>
           </div>
-          <button className='book-btn'>Book Here</button>
-        </div>
-        
-      </div>
-    </div>
 
-  <main className='mainwrap-mid overlay container'>
-  <section className='overlay container midsec'>
-    <div className='row'>
-      <div className='col-md-2'>
-        <div className='loca'>
-          <p> <img src={Location} />&nbsp; Location</p>
-          <select>
-            <option className='selctopttag'>Lagos State</option>
-            <option className='selctopt'>United States</option>
-            <option>Ogun State</option>
-          </select>
         </div>
       </div>
-      <div className='col-md-2'>
-      <div className='pers'>
-          <p> <img src={Pers} />&nbsp; Person</p>
-          <select>
-            <option className='selctopttag'>2 Person</option>
-            
-          </select>
-        </div>
-      </div>
-      <div className='col-md-2'>
-      <div className='chin'>
-          <p> <img src={chin} />&nbsp; Checkin</p>
-          <select>
-            <option className='selctopttag'>Lagos State</option>
-          
-          </select>
-        </div>
-      </div>
-      <div className='col-md-2'>
-      <div className='chout'>
-          <p> <img src={chout} /> &nbsp;Checkout</p>
-          <select>
-            <option className='selctopttag'>12 Jnauary 2022</option>
-            
-          </select>
-        </div>
-      </div>
-      <div className='col-md-4'>
-        <button className='flight-btn'>Book Trip &nbsp; <img className='planee' src={Plane} /></button>
-      </div>
-    </div>
-  </section>
-  </main>
 
-  <div className='container-fluid follow2sec'>
-    <div className='row'>
-      <div className='col-md-2'>a</div>
-      <div className='col-md-2'>s</div>
-      <div className='col-md-2'>s</div>
-      <div className='col-md-2'>s</div>
-      <div className='col-md-2'>s</div>
-      <div className='col-md-2'>s</div>
-    </div>
-  </div>
+      <main className='mainwrap-mid overlay container'>
+        <section className='overlay container midsec'>
+          <div className='row'>
+            <div className='col-md-2'>
+              <div className='loca'>
+                <p> <img src={Location} />&nbsp; Location</p>
+                <select>
+                  <option className='selctopttag'>Lagos State</option>
+                  <option className='selctopt'>United States</option>
+                  <option>Ogun State</option>
+                </select>
+              </div>
+            </div>
+            <div className='col-md-2'>
+              <div className='pers'>
+                <p> <img src={Pers} />&nbsp; Person</p>
+                <select>
+                  <option className='selctopttag'>2 Person</option>
 
+                </select>
+              </div>
+            </div>
+            <div className='col-md-2'>
+              <div className='chin'>
+                <p> <img src={chin} />&nbsp; Checkin</p>
+                <select>
+                  <option className='selctopttag'>Lagos State</option>
+
+                </select>
+              </div>
+            </div>
+            <div className='col-md-2'>
+              <div className='chout'>
+                <p> <img src={chout} /> <span>&nbsp;</span>Checkout</p>
+                <select>
+                  <option className='selctopttag'>12 Jnauary 2022</option>
+
+                </select>
+              </div>
+            </div>
+            <div className='col-md-4'>
+              <button className='flight-btn'>Book Trip &nbsp; <img className='planee' src={Plane} /></button>
+            </div>
+          </div>
+        </section>
+      </main>
+      <section>
+        <div className='container-fluid'>
+          <div className='row sect-wrap'>
+            <div className='col-md-1'></div>
+            <div className='col-md-2 col-sm-12'><img src={Nike} /></div>
+            <div className='col-md-2'><img src={Gpay} /></div>
+            <div className='col-md-2'><img src={Start} /></div>
+            <div className='col-md-2'><img src={Intrax} /></div>
+            <div className='col-md-2'><img src={Master} /></div>
+            <div className='col-md-1'></div>
+          </div>
+        </div>
+
+      </section>
+
+      <div className='sect-2under'>
+        <div className='container'>
+          <div className='row'>
+            <div className='col-md-2'></div>
+            <div className='col-md-8'>
+              <div className='thcontents'>
+                <h1 className='midtext'>Creating the best <span className='icecold'>ice-cold!</span> experience you would never forget.</h1>
+                <p className='midtxt-sub'>Would you explore nature paradise in the world, find the best destination in the world with us.</p>
+                <img className='thearrow' src={Arrow} />
+                <p className='subtext2nd'>View Regions Available</p>
+              </div>
+            </div>
+            <div className='col-md-2'></div>
+          </div>
+        </div>
+      </div>
+
+      {/* SECTION ANOTHER HERE */}
+
+      <div className='sect33'>
+        <div className='container-fluid'>
+          <div className='row'>
+            <div className='col-md-4 bg-a'>
+              {/* <img src={Bkga} /> */}
+              <h1 className='th1'>Mountain Resort</h1>
+            </div>
+            <div className='col-md-4 bg-b'>
+            <h1 className='th2'>Fuji Mountain</h1>
+            </div>
+            <div className='col-md-4 bg-c'>
+            <h1 className='th3'>Freezing WinterLake</h1>
+            </div>
+          </div>
+        </div>
+      </div>
 
 
 
